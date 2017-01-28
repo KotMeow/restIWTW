07790fea824cc9202bd900adbfee50b45e21a5f3
KotMeow <kaamil.kot@gmail.com>
2017-01-28 15:37:24 +0100

    fixed tests

--- src/test/java/com/meow/MovieServiceTest.java
+++ src/test/java/com/meow/MovieServiceTest.java
@@ -2,13 +2,10 @@ package com.meow;
 
 
 import com.meow.domain.Movie;
-import com.meow.service.MovieService;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.Assert;
-import javax.ejb.EJB;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -44,7 +41,16 @@ public class MovieServiceTest {
 
     @Test
     public void getMovie() {
+        delete("/movies/").then().assertThat().statusCode(200);
+        given().
+                formParam("title", movie.getTitle()).
+                formParam("releaseYear", movie.getReleaseYear()).
+                formParam("genre", movie.getGenre()).
+                when().
+                post("/movies/").then().assertThat().statusCode(201);
+
         id = get("/movies/").path("id[0]");
+
         when().
                 get("/movies/{id}", id).
                 then().
@@ -58,7 +64,16 @@ public class MovieServiceTest {
 
     @Test
     public void addActor() {
+        delete("/movies/").then().assertThat().statusCode(200);
+        given().
+                formParam("title", movie.getTitle()).
+                formParam("releaseYear", movie.getReleaseYear()).
+                formParam("genre", movie.getGenre()).
+                when().
+                post("/movies/").then().assertThat().statusCode(201);
+
         id = get("/movies/").path("id[0]");
+
         int size = get("/movies/{0}/actors", id).body().path("list.size()");
         when().
                 get("/movies/{0}/actors",id).
@@ -83,6 +98,19 @@ public class MovieServiceTest {
 
     @Test
     public void removeActor() {
+        delete("/movies/").then().assertThat().statusCode(200);
+        given().
+                formParam("title", movie.getTitle()).
+                formParam("releaseYear", movie.getReleaseYear()).
+                formParam("genre", movie.getGenre()).
+                when().
+                post("/movies/").then().assertThat().statusCode(201);
+        given().
+                formParam("name", "nameTest").
+                formParam("role", "roleTest").
+                formParam("movieTitle", movie.getTitle()).
+                when().
+                post("/actors/").then().assertThat().statusCode(201);
         id = get("/movies/").path("id[0]");
         int size = get("/movies/{0}/actors", id).body().path("list.size()");
         int idActor = get("/movies/{0}/actors", id).path("id[0]");
@@ -102,4 +130,32 @@ public class MovieServiceTest {
                 body("", Matchers.hasSize(size-1));
     }
 
+    @Test
+    public void  removeMovie() {
+        delete("/movies/").then().assertThat().statusCode(200);
+        given().
+                formParam("title", movie.getTitle()).
+                formParam("releaseYear", movie.getReleaseYear()).
+                formParam("genre", movie.getGenre()).
+                when().
+                post("/movies/").then().assertThat().statusCode(201);
+
+        id = get("/movies/").path("id[0]");
+        int size = get("/movies/").body().path("list.size()");
+        when().
+                get("/movies/").
+                then().
+                statusCode(200).
+                body("", Matchers.hasSize(size));
+        when().
+                delete("/movies/{id}", id).
+                then().
+                statusCode(200);
+        when().
+                get("/movies/").
+                then().
+                statusCode(200).
+                body("", Matchers.hasSize(size-1));
+
+    }
 }
